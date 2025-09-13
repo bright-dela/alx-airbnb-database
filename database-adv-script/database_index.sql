325fd52281643a39ce7989e2f56ef36ed0f17e26
bright-settor <brightdelasettor@gmail.com>
2025-09-13 13:05:21 +0000

    Create database_index.sql

---
+++ database-adv-script/database_index.sql
@@ -0,0 +1,5 @@
+CREATE INDEX idx_bookings_user_id ON Bookings (user_id);
+CREATE INDEX idx_reviews_property_id ON Reviews (property_id);
+CREATE INDEX idx_properties_user_id ON Properties (user_id);
+
+
